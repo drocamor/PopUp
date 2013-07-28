6dd99e1d15dbc55fa2f63ea46bd0035e187186f7
David Rocamora <david.rocamora@controlgroup.com>
2013-07-28 14:33:58 -0400

    Uses snapshots primarily now
    If the volume is in the same AZ, attach it (no worries). If not, then that AZ may be broken so we should recreate the vol from a snapshot. This relies on constant snapshots, but prevents problems where a volume is completely unavailable and there is no backup.

--- popup-ebs-minder.py
+++ popup-ebs-minder.py
@@ -2,25 +2,45 @@
 
 import boto
 import time
+import urllib
+
+class EC2Metadata:
+    """Class for querying metadata from EC2"""
+
+    def __init__(self, addr='169.254.169.254', api='latest'):
+        self.addr = addr
+        self.api = api
+
+    def _get(self, uri):
+        url = 'http://%s/%s/%s/' % (self.addr, self.api, uri)
+        value = urllib.urlopen(url).read()
+        if "404 - Not Found" in value:
+            return None
+
+        return value
+    
+    def instance_id(self):
+        return self._get('meta-data/instance-id')
+
+    def availability_zone(self):
+        return self._get('meta-data/placement/availability-zone')
+
 
 # parse arguments:
 # - Volume Tag
 popup_volume_id = 'DrocamorHomeDirectory'
 
 ec2 = boto.connect_ec2()
+instance_metadata = EC2Metadata()
 
 # Get our AZ and instance ID
-instance_az = 'us-east-1b'
-instance_id = 'asdfasdf'
+instance_az = instance_metadata.availability_zone()
+instance_id = instance_metadata.instance_id()
 device_id = '/dev/sdf'
-
 
 # Find volumes that have the Volume Tag, pick the most recently created one.
 volumes = ec2.get_all_volumes(filters={'tag:popup-volume-id': popup_volume_id,'status': 'available'})
 volume = sorted(volumes, key=lambda v: v.create_time)[-1]
-
-# Get the volume AZ
-# 
 
 # If the volume is in our AZ, attach it and exit
 if volume.zone == instance_az:
@@ -28,16 +48,13 @@ if volume.zone == instance_az:
     volume.attach(instance_id, device_id)
 else:
     print "The volume is in another AZ."
-    # If the volume is not in our AZ:
-    # - Take a snapshot
-    print "Creating snapshot..."
-    snapshot = volume.create_snapshot(description='Temporary PopUp snapshot')
-    print "Snapshot id is %s" % snapshot.id
-    while snapshot.status != 'completed':
-        print "Sleeping for 5 seconds for snapshot creation..."
-        time.sleep(5)
-        snapshot.update()
-    # - Create a volume from the snapshot in our AZ
+    # Find the most recent snapshot
+    existing_snaps = ec2.get_all_snapshots(filters={'tag:popup-volume-id': popup_volume_id})
+    sorted_snaps = sorted(existing_snaps, key=lambda s: s.start_time)
+    
+    snapshot = sorted_snaps[-1]
+
+    # Create a volume from the snapshot in our AZ
     print "Creating volume..."
     new_volume = snapshot.create_volume(instance_az)
     new_volume.add_tag('popup-volume-id', value=popup_volume_id)
@@ -46,11 +63,13 @@ else:
         print "Sleeping for 5 seconds for volume creation..."
         time.sleep(5)
         new_volume.update()
-    # - Attach the volume to our instance
+
+    # Attach the volume to our instance
     print "Attaching new volume..."
     new_volume.attach(instance_id, device_id)
-    # - Delete the old volume and snapshot
-    print "Deleting old volume and snapshot"
+
+    # Delete the old volume
+    print "Deleting old volume..."
     volume.delete()
-    snapshot.delete()
+
 
